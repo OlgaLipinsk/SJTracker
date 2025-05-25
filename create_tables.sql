81727aa9c9d1ba569871d650c43dee6fddc71ab6
OlgaLipinsk <lipinskayavolha@gmail.com>
2025-05-25 17:39:56 +0200

    Table creation script added

---
+++ create_tables.sql
@@ -0,0 +1,108 @@
+-- skill
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.skill`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.skill`
+(
+  skill_id STRING NOT NULL,
+  skill_name STRING,
+  category STRING
+);
+
+-- keyword
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.keyword`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.keyword`
+(
+  keyword_id STRING NOT NULL,
+  word STRING
+);
+
+-- vacancy_keyword
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.vacancy_keyword`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.vacancy_keyword`
+(
+  vacancy_id STRING NOT NULL,
+  keyword_id STRING NOT NULL
+);
+
+-- vacancy_location
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.vacancy_location`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.vacancy_location`
+(
+  vacancy_id STRING NOT NULL,
+  location_id STRING NOT NULL
+);
+
+-- contact
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.contact`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.contact`
+(
+  contact_id STRING NOT NULL,
+  name STRING,
+  email STRING,
+  phone STRING
+);
+
+-- vacancy_contact
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.vacancy_contact`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.vacancy_contact`
+(
+  vacancy_id STRING NOT NULL,
+  contact_id STRING NOT NULL
+);
+
+-- location
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.location`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.location`
+(
+  location_id STRING NOT NULL,
+  postcode STRING,
+  kommune STRING
+);
+
+-- vacancy_skill
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.vacancy_skill`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.vacancy_skill`
+(
+  vacancy_id STRING NOT NULL,
+  skill_id STRING NOT NULL
+);
+
+-- employer
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.employer`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.employer`
+(
+  employer_id STRING NOT NULL,
+  name STRING,
+  industry STRING
+);
+
+-- vacancy
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.vacancy`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.vacancy`
+(
+  vacancy_id STRING NOT NULL,
+  title STRING,
+  text STRING,
+  type STRING,
+  deadline DATE,
+  url STRING,
+  employer_id STRING
+);
+
+-- staging_vacancy
+DROP TABLE IF EXISTS `leafy-sanctuary-458206-f8.ProjectDB.staging_vacancy`;
+CREATE TABLE `leafy-sanctuary-458206-f8.ProjectDB.staging_vacancy`
+(
+  title STRING,
+  text STRING,
+  type STRING,
+  deadline DATE,
+  url STRING,
+  employer_name STRING,
+  skill_list ARRAY<STRING>,
+  keyword_list ARRAY<STRING>,
+  contact_name STRING,
+  contact_email STRING,
+  contact_phone STRING,
+  location_postcode STRING,
+  location_kommune STRING
+);
